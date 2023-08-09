4d24013bd3b1fb05f42f65bcf431089f2bb5113a
germansanz93 <germansanz93@gmail.com>
2023-08-09 06:43:18 +0200

    add participants section

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -84,16 +84,18 @@ def home(request):
 def room(request, pk):
   room = Room.objects.get(id=pk)
   room_messages = room.message_set.all().order_by('-created') #El set de mensajes asociados a esa room
-  
+  participants = room.participants.all()
+
   if request.method == 'POST':
     message = Message.objects.create(
       user = request.user,
       room = room,
       body = request.POST.get('body')
     )
+    room.participants.add(request.user)
     return redirect('room', pk=room.id)
   
-  context = {'room': room, 'room_messages': room_messages}
+  context = {'room': room, 'room_messages': room_messages, 'participants': participants}
   return render(request, 'base/room.html', context)
 
 @login_required(login_url='login')
