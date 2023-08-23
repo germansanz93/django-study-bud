ef2b9d0ace21e5d3e4dcf5111987a80c14b6cb70
germansanz93 <germansanz93@gmail.com>
2023-08-23 09:51:31 +0200

    fixed room form

--- studybud/base/forms.py
+++ studybud/base/forms.py
@@ -5,3 +5,4 @@ class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = '__all__' #Indicamos que queremos un campo por cada valor (solo por el momento)
+        exclude = ['host', 'participants']
